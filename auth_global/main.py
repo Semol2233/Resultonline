2331982dc1c64b3b4c9cd15131a24a91545c98a1
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-03-26 16:11:45 +0600

    dd

--- auth_global/main.py
+++ auth_global/main.py
@@ -18,8 +18,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ]
+    # 'DEFAULT_PARSER_CLASSES': [
+    #     'rest_framework.parsers.JSONParser',
+    # ]
 
 }
