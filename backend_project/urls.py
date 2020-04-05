35ddcd1021a7e9b53acde95f620d678ce3753e6c
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-04-05 17:38:13 +0600

    chnage somthing

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_simplejwt import views as jwt_views
+# from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('simple',include('account_admin_app.urls')),
