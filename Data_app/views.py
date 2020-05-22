66bba76d962342f877147f6b0d2896062bbf247e
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-05-22 15:07:56 +0600

    jdhfhedf

--- Data_app/views.py
+++ Data_app/views.py
@@ -48,7 +48,7 @@ User = get_user_model()
 
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 3
+    page_size = 4
     page_size_query_param = 'page_size'
     max_page_size = 100
 
