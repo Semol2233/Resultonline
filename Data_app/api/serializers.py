696d2e9afd368048da61e9c679f76ef035686429
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-06-22 23:07:44 +0600

    dhfhdssssssdf

--- Data_app/api/serializers.py
+++ Data_app/api/serializers.py
@@ -435,10 +435,9 @@ class tagmanager(serializers.ModelSerializer):
        
 
 class tag_manager_serilizar(serializers.ModelSerializer):
-
+     tag_creator         = tag_data_crators(read_only=True,many=True, required=False)
      tag_channel_name       = tagmanager(read_only=True)
-     
-     
+    
      class Meta:
         model = tag_data
         fields = [
@@ -447,6 +446,7 @@ class tag_manager_serilizar(serializers.ModelSerializer):
            'query_slug',
            'tag_content_link',
            'tag_channel_name',
+           'tag_creator'
         ]
         read_only_fields = ['tag_channel_name']
         read_only_fields = ['tag_name']
