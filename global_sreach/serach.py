76048c4a74ee8b454b848ffde5f8b7eaa378b163
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-07-24 13:30:24 +0600

    searcejeh

--- global_sreach/serach.py
+++ global_sreach/serach.py
@@ -45,28 +45,15 @@ class searcsssh_filter(APIView, PaginationHandlerMixin):
         filter_postmodel_q = postmodel_q.objects.filter(title__icontains=query,awnsr_qna__icontains=query,details__icontains=query).values()
         if filter_postmodel_q:
             for p in filter_postmodel_q:
-             data = {"targetUrl": {
-                    "url":"/q&a/api/v1/dtls/",
-                    "page_name":"q&a"
-                }}
-             result.append(( p,data))
+             result.append(p)
         filter_PostCreate = PostCreate.objects.filter(title__icontains=query,details__icontains=query).values()
         if filter_PostCreate:
             for b in filter_PostCreate:
-                data = {"targetUrl": {     
-                    "url":"/count/",
-                    "page_name":"home_page"
-                }}
-                result.append(( b,data))
+                result.append(b)
         filter_postmodel = postmodel.objects.filter(title__icontains=query,details__icontains=query).values()
         if filter_postmodel:
             for f in filter_postmodel:
-                data = {"targetUrl": {
-
-                    "url":"/blog/api/v1/details/",
-                    "page_name":"Blog_page"
-                }}
-                result.append(( f,data))
+                result.append(f)
             page = self.paginate_queryset(result)
             result = page
             paginated_response = self.get_paginated_response(result)
@@ -91,3 +78,49 @@ class searcsssh_filter(APIView, PaginationHandlerMixin):
 #         for a in filter_author:
 #             result.append(a)
 #     return JsonResponse(result, safe=False)
+
+
+
+
+
+# class qa_pagenation(pagination.PageNumberPagination):
+#     page_size = 10
+#     page_size_query_param = 'page_size'
+#     max_page_size = 100
+
+# class searcsssh_filter(APIView, PaginationHandlerMixin):
+#     pagination_class = qa_pagenation
+
+#     def get(self,request,query,*args, **kwargs):
+#         result = []
+#         filter_postmodel_q = postmodel_q.objects.filter(title__icontains=query,awnsr_qna__icontains=query,details__icontains=query).values()
+#         if filter_postmodel_q:
+#             for p in filter_postmodel_q:
+#              data = {"targetUrl": {
+#                     "url":"/q&a/api/v1/dtls/",
+#                     "page_name":"q&a"
+#                 }}
+#              result.append(( p,data))
+#         filter_PostCreate = PostCreate.objects.filter(title__icontains=query,details__icontains=query).values()
+#         if filter_PostCreate:
+#             for b in filter_PostCreate:
+#                 data = {"targetUrl": {     
+#                     "url":"/count/",
+#                     "page_name":"home_page"
+#                 }}
+#                 result.append(( b,data))
+#         filter_postmodel = postmodel.objects.filter(title__icontains=query,details__icontains=query).values()
+#         if filter_postmodel:
+#             for f in filter_postmodel:
+#                 data = {"targetUrl": {
+
+#                     "url":"/blog/api/v1/details/",
+#                     "page_name":"Blog_page"
+#                 }}
+#                 result.append(( f,data))
+#             page = self.paginate_queryset(result)
+#             result = page
+#             paginated_response = self.get_paginated_response(result)
+#             return JsonResponse(paginated_response.data, safe=False)
+#         return HttpResponse('No matching data found', status=404)
+
