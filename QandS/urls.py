b9d655fe5ff8da6988b7ea7c7c9b770371bf434a
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-07-26 12:34:19 +0600

    jgssysgjjhluoj

--- QandS/urls.py
+++ QandS/urls.py
@@ -9,12 +9,12 @@ from django.conf import settings
 
 urlpatterns = [
      path('', qanda_root.as_view()),
-     path('channel/<category>', PaginatedProjectsAPIView.as_view()),
-     path('short_list', channel_Dataapi.as_view()),
-     path('dtls/<slug>', dtls_api_qna_view.as_view()),
-     path('q_related_data/<catagry__publisher>', dtls_apwi_qna_view.as_view()),
-     path('qanda_home', qanda_home_card.as_view()),
-     path('data_filter_shot_list', qanda_shotlist_data_filter.as_view()),
+     path('channel/<category>/', PaginatedProjectsAPIView.as_view()),
+     path('short_list/', channel_Dataapi.as_view()),
+     path('dtls/<slug>/', dtls_api_qna_view.as_view()),
+     path('q_related_data/<catagry__publisher>/', dtls_apwi_qna_view.as_view()),
+     path('qanda_home/', qanda_home_card.as_view()),
+     path('data_filter_shot_list/', qanda_shotlist_data_filter.as_view()),
 
 
 
