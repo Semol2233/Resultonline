53cad815a806edd81f36ff9b770183b29053b14d
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-07-26 18:39:32 +0600

    sitp

--- global_sreach/serach.py
+++ global_sreach/serach.py
@@ -42,7 +42,7 @@ class seeearcsssh_filter(APIView, PaginationHandlerMixin):
 
     def get(self,request,query):
         result = []
-        filter_postmodel_q = postmodel_q.objects.filter(Q(title__contains=query) | Q(details__contains=query).values('title','details','photo')
+        filter_postmodel_q = postmodel_q.objects.filter(Q(title__contains=query) | Q(details__contains=query)
         if filter_postmodel_q:
             for p in filter_postmodel_q:
              data = {"targetUrl": {
@@ -50,7 +50,7 @@ class seeearcsssh_filter(APIView, PaginationHandlerMixin):
                     "page_name":"q&a"
                 }}
              result.append(( p,data))
-        filter_PostCreate = PostCreate.objects.filter(Q(title__contains=query) | Q(details__contains=query).values('title','details','photo')
+        filter_PostCreate = PostCreate.objects.filter(Q(title__contains=query) | Q(details__contains=query)
         if filter_PostCreate:
             for b in filter_PostCreate:
                 data = {"targetUrl": {     
@@ -58,7 +58,7 @@ class seeearcsssh_filter(APIView, PaginationHandlerMixin):
                     "page_name":"home_page"
                 }}
                 result.append(( b,data))
-        filter_postmodel = postmodel.objects.filter(Q(title__contains=query) | Q(details__contains=query).values('title','details','photo')
+        filter_postmodel = postmodel.objects.filter(Q(title__contains=query) | Q(details__contains=query)
         if filter_postmodel:
             for f in filter_postmodel:
                 data = {"targetUrl": {
