5bda42c6e92ed5d9f8325cf63ab2a72e3bfd1f60
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-07-30 22:37:20 +0600

    samyoie

--- QandS/views.py
+++ QandS/views.py
@@ -138,8 +138,7 @@ class qa_pagenasssstion(pagination.PageNumberPagination):
     max_page_size = 100
 
 class qanda_root(generics.ListAPIView):
-    # queryset               = cat_model_q.objects.filter(created_date__gte=datetime.now() - timedelta(days=55)).order_by('-id')
-    queryset               = cat_model_q.objects.all()
+    queryset               = cat_model_q.objects.filter(created_date__gte=datetime.now() - timedelta(days=55))
     serializer_class       = cat_modelSrtilizer
     pagination_class       = qa_pagenasssstion
 
