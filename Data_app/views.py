f5b965d71261a61e217c69b97cd1db34e4f3548b
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-08-22 18:11:08 +0600

    stuiook

--- Data_app/views.py
+++ Data_app/views.py
@@ -171,7 +171,7 @@ class Latest_data(generics.ListAPIView):
 
 
 class StandadrdResultsSetPdagfination(pagination.PageNumberPagination):
-    page_size = 9
+    page_size = 2
     page_size_query_param = 'page_size'
     max_page_size = 100
 
